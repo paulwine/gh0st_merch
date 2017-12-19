8fc2df25540f239b373090da893a4622bb56f278
Paul <paulwinegard@gmail.com>
2017-12-19 13:04:49 -0800

    Routes established, properly linked.  Basic login and registration started. Not yet interacting with database

--- apps/merch/urls.py
+++ apps/merch/urls.py
@@ -2,5 +2,11 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index)
+    url(r'^$', views.index),
+    url(r'^login$', views.login),
+    url(r'^register$', views.register),
+    url(r'^main$', views.main),
+    url(r'^logprocess$', views.logprocess),
+    url(r'^regprocess$', views.regprocess),
+    url(r'^logout$', views.logout)
 ]
--- apps/merch/views.py
+++ apps/merch/views.py
@@ -4,6 +4,27 @@ from __future__ import unicode_literals
 from django.shortcuts import render, redirect
 
 def index(request):
+    request.session["init"] = True
+    if "user_id" in request.session:
+        return redirect("/main")
+    
     return render(request, "merch/index.html")
+
+def login(request):
+    return render(request, "merch/log_reg.html")
+def logprocess(request):
+    return redirect("/main")
+
+
+def register(request):
+    return render(request, "merch/log_reg.html")
+def regprocess(request):
+    return redirect("/main")
+
+def main(request):
+    return render(request, "merch/main.html")
+
+def logout(request):
+    return redirect("/")
 
 # Create your views here.
