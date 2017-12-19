93b48ce7ebe130327575f6cc4622cd64dfe47029
Paul <paulwinegard@gmail.com>
2017-12-19 12:24:27 -0800

    External fonts loaded.  CSS updated.  Models created and migrations made.  Soon to initialize

--- apps/merch/models.py
+++ apps/merch/models.py
@@ -3,4 +3,44 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+class User(models.Model):
+    admin_level = models.IntegerField(default = 0)
+    name = models.CharField(max_length = 255)
+    alias = models.CharField(max_length = 255)
+    email = models.CharField(max_length = 255)
+    password = models.CharField(max_length = 255)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+
+class Size(models.Model):
+    dimensions = models.CharField(max_length=255)
+    price = models.FloatField()
+
+class Print(models.Model):
+    image_url = models.CharField(max_length = 255)
+    name = models.CharField(max_length=255)
+    size = models.ForeignKey(Size,related_name = "size")
+    description = models.CharField(max_length = 255)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+
+
+
+class Song(models.Model):
+    song_url = models.CharField(max_length = 255)
+    name = models.CharField(max_length = 255)
+    price = models.FloatField()
+    duration = models.DurationField()
+    description = models.CharField(max_length = 255)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+
+class Video(models.Model):
+    video_url = models.CharField(max_length = 255)
+    name = models.CharField(max_length = 255)
+    duration = models.DurationField()
+    description = models.CharField(max_length = 255)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+
 # Create your models here.
